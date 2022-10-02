cec6ae99891bad6ced4306c0b39a4b0b8ad9b0d5
Shadow <mcdazed@protonmail.com>
2022-10-02 16:39:46 -0700

    commit
    Former-commit-id: 0db50c8ff78e6f9f69a3bb20d788d8a5dfee2a87
    Former-commit-id: dfb853b39129846286cb9b1b5df8593aacf8deaa

--- 2ndRenameProfile.bat
+++ 2ndRenameProfile.bat
@@ -2,8 +2,8 @@
 set ROOTDIR=%~dp0
 set ROOTDIR=%ROOTDIR:~0,-1%
 cd /d "%ROOTDIR%"
-for %%I in (.) do set CurrDirName=%%~nxI
-echo %CurrDirName%
+for %%I in (.) do set OldName=%%~nxI
+echo %OldName%
 if not "%1"=="am_admin" (
     powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
     exit /b
@@ -15,13 +15,14 @@ net stop DbxSvc
 sc stop DbxSvc
 
 echo Enter new profile name:
-set /p name=	-)
-setx a3name "%name%"
-cd /d "%USERPROFILE%/Documents/Arma 3 - Other Profiles/%CurrDirName%"
-del /s /q /f %CurrDirName%.vars.Arma3Profile
-ren %CurrDirName%.Arma3Profile %name%.Arma3Profile
-ren %CurrDirName%.3den.Arma3Profile %name%.3den.Arma3Profile
+set /p NewName=	-)
+setx a3name "%NewName%"
 cd /d "%ROOTDIR%"
+del /s /q /f .vars.Arma3Profile
+del /s /q /f %NewName%.Arma3Profile
+del /s /q /f %NewName%.3den.Arma3Profile
+ren %OldName%.Arma3Profile %NewName%.Arma3Profile
+ren %OldName%.3den.Arma3Profile %NewName%.3den.Arma3Profile
 cd ..\
-ren %CurrDirName% %name%
+ren %OldName% %NewName%
 pause
