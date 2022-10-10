c1b08b3ce1356cbd938d191ca037e1e57d1c1b4d
Shadow <mcdazed@protonmail.com>
2022-10-10 06:16:47 -0700

    commit
    Former-commit-id: 8645def85cb61789fb98fb1a61f0cfd07f7c295a
    Former-commit-id: d6a0ddffe417d71a88e9a0d582d031e251494a59

---
+++ Mods/@Realengine/meta.cpp
@@ -0,0 +1,4 @@
+protocol = 1;
+publishedid = 2127693591;
+name = "Real Engine";
+timestamp = 5249226607196336525;
---
+++ Mods/CleanPBO.bat
@@ -0,0 +1,52 @@
+@echo off
+set ROOTDIR=%~dp0
+set ROOTDIR=%ROOTDIR:~0,-1%
+cd /d "%ROOTDIR%"
+for %%I in (.) do set CurrDirName=%%~nxI
+echo %CurrDirName%
+if not "%1"=="am_admin" (
+    powershell -Command "Start-Process -Verb RunAs -FilePath '%0' -ArgumentList 'am_admin'"
+    exit /b
+)
+
+taskkill /f /im "Dropbox.exe"
+taskkill /f /im "DropboxUpdate.exe"
+net stop DbxSvc
+sc stop DbxSvc
+
+cd /d "%ROOTDIR%"
+del /s /q /f *.bisign
+:: ACE
+del /s /q /f *ace_.vs.pbo
+del /s /q /f *ace_ai.pbo
+del /s /q /f *ace_aircraft.pbo
+del /s /q /f *ace_fcs.pbo
+del /s /q /f *ace_fire.pbo
+del /s /q /f *ace_hearing.pbo
+del /s /q /f *ace_intelitems.pbo
+del /s /q /f *ace_advanced_fatigue.pbo
+del /s /q /f *ace_gforces.pbo
+del /s /q /f *ace_minedetector.pbo
+del /s /q /f *ace_nametags.pbo
+del /s /q /f *ace_norearm.pbo
+del /s /q /f *ace_noradio.pbo
+del /s /q /f *ace_marker*.pbo
+del /s /q /f *ace_map*.pbo
+del /s /q /f *ace_medical*.pbo
+del /s /q /f *ace_quickmount.pbo
+del /s /q /f *ace_field_rations.pbo
+del /s /q /f *ace_volume.pbo
+del /s /q /f *ace_zeus.pbo
+:: Realengine
+del /s /q /f *real_animations*.pbo
+del /s /q /f *real_armor.pbo
+del /s /q /f *real_fix_ui.pbo
+del /s /q /f *real_fix_terrains.pbo
+del /s /q /f *real_fix_factions.pbo
+del /s /q /f *real_fix_weapons.pbo
+del /s /q /f *real_uniform_unlock.pbo
+del /s /q /f *real_sfx_inventory_sounds.pbo
+
+cd /d "%arma3% Tools/DSSignFile"
+
+dsutils.exe
