8be156e71b2c91b14309c17f6de3f61315501af0
Shadow <mcdazed@protonmail.com>
2022-10-20 19:00:48 -0700

    commit
    Former-commit-id: 9c7cf09113fa0caf8c9d9aef67a1bfe1ddd4017f
    Former-commit-id: e55f9f793abcc6c2daaef1b48a4e9f9783e0b61a

---
+++ Mods/@factions/addons/bserbia/script_component.hpp
@@ -0,0 +1,13 @@
+#define COMPONENT bserbia
+#define COMPONENT_BEAUTIFIED bserbia
+#include "\x\\addons\main\script_mod.hpp"
+// #define DEBUG_MODE_FULL
+// #define DISABLE_COMPILE_CACHE
+// #define ENABLE_PERFORMANCE_COUNTERS
+#ifdef DEBUG_ENABLED_BSERBIA
+#define DEBUG_MODE_FULL
+#endif
+#ifdef DEBUG_SETTINGS_BSERBIA
+#define DEBUG_SETTINGS DEBUG_SETTINGS_BSERBIA
+#endif
+#include "\x\\addons\main\script_macros.hpp"
---
+++ Mods/@factions/addons/bserbia_w/config.cpp
@@ -0,0 +1,4 @@
+#define mag_xx(a,b) class _xx_##a {magazine = ##a; count = b;}
+#define weap_xx(a,b) class _xx_##a {weapon = ##a; count = b;}
+#define item_xx(a,b) class _xx_##a {name = a; count = b;}
+#include "CfgVehicles.hpp"
