c2376278371521b7c04652cbe540a6ca90964b9c
Shadow <mattmacdonald112@gmail.com>
2022-12-30 18:13:55 -0800

    commit
    Former-commit-id: e3319147ad7d2536d9fb0b9849492ffb05448c06

--- Mods/CleanPBO.bat
+++ Mods/CleanPBO.bat
@@ -13,7 +13,6 @@ taskkill /f /im "Dropbox.exe"
 cd /d "%ROOTDIR%"
 del /s /q /f *.bisign
 :: ACE
-::del /s /q /f *ace_ai.pbo
 del /s /q /f *ace_aircraft.pbo
 del /s /q /f *ace_fcs.pbo
 del /s /q /f *ace_fire.pbo
@@ -24,21 +23,20 @@ del /s /q /f *ace_gforces.pbo
 del /s /q /f *ace_minedetector.pbo
 del /s /q /f *ace_nametags.pbo
 del /s /q /f *ace_norearm.pbo
-del /s /q /f *ace_noradio.pbo
 del /s /q /f *ace_marker*.pbo
 del /s /q /f *ace_map*.pbo
-::del /s /q /f *ace_medical*.pbo
 del /s /q /f *ace_quickmount.pbo
 del /s /q /f *ace_field_rations.pbo
 del /s /q /f *ace_volume.pbo
 del /s /q /f *ace_zeus.pbo
 :: Realengine
-del /s /q /f *real_animations*.pbo
+:: del /s /q /f *real_fix_weapons.pbo
 del /s /q /f *real_armor.pbo
+del /s /q /f *real_animations*.pbo
+del /s /q /f *real_fix_factions.pbo
+del /s /q /f *real_fix_flare_lights.pbo
 del /s /q /f *real_fix_ui.pbo
 del /s /q /f *real_fix_terrains.pbo
-del /s /q /f *real_fix_factions.pbo
-del /s /q /f *real_fix_weapons.pbo
 del /s /q /f *real_uniform_unlock.pbo
 
 cd /d "%arma3% Tools\DSSignFile"
