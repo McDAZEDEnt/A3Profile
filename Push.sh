98dffc644184b72904d11f38ad289bf8f683fce5
Shadow <mattmacdonald112@gmail.com>
2023-01-01 12:57:58 -0800

    commit

--- Push.sh
+++ Push.sh
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:6f249b002beef59d4123ee5d45b215b6f433b44ce9162a247493266b20f5c248
-size 89
+oid sha256:40548de36b738aa5a6641a834930ab21b99fc29ba261d1d665ba8239fedbe933
+size 99
