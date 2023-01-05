e094b2e1af042ac715feefb1753f8a81b0edee28
Shadow <mattmacdonald112@gmail.com>
2023-01-04 20:21:27 -0800

    commit
    Former-commit-id: 2252a93239c31021ce2019e898c072388b1c1dcb

--- Mods/cloneAntistasi.bat
+++ Mods/cloneAntistasi.bat
@@ -3,5 +3,6 @@ git clone --depth 1 --branch 3.0.0 https://github.com/official-antistasi-communi
 
 cd /d A3-Antistasi
 del /s /q /f .gitattributes
+del /s /q /f .gitignore
 rd /s /q .git
 rd /s /q .github
--- Mods/cloneCBA.bat
+++ Mods/cloneCBA.bat
@@ -3,5 +3,6 @@ git clone --depth 1 --branch v3.15.8.220912 https://github.com/CBATeam/CBA_A3.gi
 
 cd /d CBA_A3
 del /s /q /f .gitattributes
+del /s /q /f .gitignore
 rd /s /q .git
 rd /s /q .github
