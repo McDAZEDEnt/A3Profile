a1a3d324f706075f5ff487291a3204bb9dcda927
Shadow <mattmacdonald112@gmail.com>
2023-01-18 12:25:39 -0800

    commit

--- Mods/workshopNoSpaces.ps1
+++ Mods/workshopNoSpaces.ps1
@@ -4,15 +4,20 @@ Get-ChildItem * -Filter "*`[*`]*" | Rename-Item -NewName { $_.name -replace '\['
 
 get-childitem -directory | foreach {rename-item $_ $_.name.replace(")","")}
 
+get-childitem -directory | foreach {rename-item $_ $_.name.replace("(","")}
+
 get-childitem -directory | foreach {rename-item $_ $_.name.replace("-","")}
 
-get-childitem -directory | foreach {rename-item $_ $_.name.replace("!","")}
-
 get-childitem -directory | foreach {rename-item $_ $_.name.replace("'","")}
+
+get-childitem -directory | foreach {rename-item $_ $_.name.replace("!","")}
 
 get-childitem -directory | foreach {rename-item $_ $_.name.replace(".","")}
 
 get-childitem -directory | foreach {rename-item $_ $_.name.replace(" ","_")}
 
+get-childitem -directory | foreach {rename-item $_ $_.name.replace("__","_")}
+
+get-childitem -directory | foreach {rename-item $_ $_.name.replace("___","_")}
 
 pause
