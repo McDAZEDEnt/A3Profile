71943fac4932cbf8857645525fcdea282bb4a1c6
unknown <git@mcdazed.com>
2023-01-31 13:55:34 -0800

    commit

--- Mods/Publish.bat
+++ Mods/Publish.bat
@@ -11,10 +11,25 @@ if not "%1"=="am_admin" (
 
 start "" "steam://rungameid/233800"
 
+pause
+
 Powershell.exe -executionpolicy remotesigned -File "%ROOTDIR%\A3-Antistasi-Plus\Tools\Builder\buildAddons.ps1
 robocopy "%ROOTDIR%/A3-Antistasi-Plus/build/A3A-Plus" "%ROOTDIR%/A3-Antistasi-Plus/A3A" *.* /S
 
 pause
+
+cd /d "P:\z\NR6\addons"
+"%arma3% tools/AddonBuilder/AddonBuilder.exe" "P:\z\NR6\addons\nr6_airreinforcements" "P:\z\NR6\addons" -packonly -sign="%userprofile%\Dropbox\Save\_Stray\arma.biprivatekey"
+"%arma3% tools/AddonBuilder/AddonBuilder.exe" "P:\z\NR6\addons\nr6_alice2" "P:\z\NR6\addons" -packonly -sign="%userprofile%\Dropbox\Save\_Stray\arma.biprivatekey"
+"%arma3% tools/AddonBuilder/AddonBuilder.exe" "P:\z\NR6\addons\nr6_hal" "P:\z\NR6\addons" -packonly -sign="%userprofile%\Dropbox\Save\_Stray\arma.biprivatekey"
+"%arma3% tools/AddonBuilder/AddonBuilder.exe" "P:\z\NR6\addons\nr6_reinforcements" "P:\z\NR6\addons" -packonly -sign="%userprofile%\Dropbox\Save\_Stray\arma.biprivatekey"
+"%arma3% tools/AddonBuilder/AddonBuilder.exe" "P:\z\NR6\addons\nr6_sitemarkers" "P:\z\NR6\addons" -packonly -sign="%userprofile%\Dropbox\Save\_Stray\arma.biprivatekey"
+"%arma3% tools/AddonBuilder/AddonBuilder.exe" "P:\z\NR6\addons\nr6_sites" "P:\z\NR6\addons" -packonly -sign="%userprofile%\Dropbox\Save\_Stray\arma.biprivatekey"
+"%arma3% tools/AddonBuilder/AddonBuilder.exe" "P:\z\NR6\addons\nr6_tools" "P:\z\NR6\addons" -packonly -sign="%userprofile%\Dropbox\Save\_Stray\arma.biprivatekey"
+"%arma3% tools/AddonBuilder/AddonBuilder.exe" "P:\z\NR6\addons\nr6_zbe_cache" "P:\z\NR6\addons" -packonly -sign="%userprofile%\Dropbox\Save\_Stray\arma.biprivatekey"
+
+cd /d "P:\z\ace\tools"
+python make.py
 
 cd /d "%arma3% Tools\Publisher"
 
