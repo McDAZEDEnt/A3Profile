925475788ec71d2de2f9229ec0597a033ec6de77
paperspace <git@mcdazed.com>
2023-06-07 12:57:37 -0600

    commit

--- 3rdSetup.bat
+++ 3rdSetup.bat
@@ -23,9 +23,23 @@ echo .
 echo A white pop up will appear after you press 'Enter'.
 echo Answer 'Yes' to the prompt, and confirm the changes.
 echo .
-timeout /t 12 /nobreak
+pause
 cd /d "%ROOTDIR%"
 hugepages.reg
+:: make profile from template
+cd /d "%userprofile%/Documents"
+mkdir "Arma 3"
+cd /d "%userprofile%/Documents/Arma 3"
+del /s /q /f "%username%.Arma3Profile"
+del /s /q /f "%username%.3den.Arma3Profile"
+rd /s /q "Saved"
+rd /s /q "UserSaved"
+cd /d "%ROOTDIR%"
+del *.Arma3Profile
+del *.vars
+robocopy "%ROOTDIR%/userconfig" "%ROOTDIR%" *.Arma3Profile
+ren "config.Arma3Profile" "%CurrDirName%.Arma3Profile"
+ren "config.3den.Arma3Profile" "%CurrDirName%.3den.Arma3Profile"
 :: Repair in launcher
 cls
 echo .
@@ -41,10 +55,13 @@ echo (This is normal behaviour, you can safely ignore it.)
 echo 4. Close the Launcher
 echo.
 start steam://rungameid/107410
-timeout /t 12 /nobreak
 pause
 TASKKILL /F /IM steam.exe
 TASKKILL /F /IM arma3launcher.exe
+:: UNHIDE !WORKSHOP & Copy missing keys
+cd /d "%arma3%"
+attrib -h "%arma3%\!Workshop"
+for /r %%d in (*.bikey) do copy "%%d" "%userprofile%\Documents\Arma 3 - Other Profiles\%a3name%\userconfig\keys"
 :: userconfig and mission symlink
 cd /d "%arma3%"
 rmdir /s /q "%arma3%/userconfig"
@@ -55,26 +72,6 @@ rmdir /s /q "%ROOTDIR%/z"
 mklink /J "%arma3%/userconfig" "%ROOTDIR%/userconfig"
 mklink /J "%arma3%/mpmissions" "%ROOTDIR%/mpmissions"
 mklink /J "%arma3%/keys" "%ROOTDIR%/userconfig/keys"
-:: UNHIDE !WORKSHOP & Copy missing keys
-cd /d "%arma3%"
-attrib -h "%arma3%\!Workshop"
-rd /s /q "%userprofile%\Documents\Arma 3 - Other Profiles\%a3name%\userconfig\keys"
-mkdir "%userprofile%\Documents\Arma 3 - Other Profiles\%a3name%\userconfig\keys"
-for /r %%d in (*.bikey) do copy "%%d" "%userprofile%\Documents\Arma 3 - Other Profiles\%a3name%\userconfig\keys"
-:: ROBOCOPY "%ROOTDIR%/userconfig/!Workshop" "%arma3%/!Workshop" *.bisign /E
-:: make profile from template
-cd /d "%userprofile%/Documents"
-mkdir "Arma 3"
-cd /d "%userprofile%/Documents/Arma 3"
-del /s /q /f "%username%.Arma3Profile"
-del /s /q /f "%username%.3den.Arma3Profile"
-rd /s /q "Saved"
-rd /s /q "UserSaved"
-cd /d "%ROOTDIR%"
-del *.Arma3Profile
-del *.vars
-robocopy "%ROOTDIR%/userconfig" "%ROOTDIR%" *.Arma3Profile
-ren "config.Arma3Profile" "%CurrDirName%.Arma3Profile"
-ren "config.3den.Arma3Profile" "%CurrDirName%.3den.Arma3Profile"
-call FoV.exe
+:: FOV
+call "%userprofile%\Documents\Arma 3 - Other Profiles\%a3name%\FoV.exe"
 EXIT
